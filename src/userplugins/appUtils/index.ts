3ca2d712f81f476f93cbe012b4d119ebc46f5dbe
Ryan McCoy <59570607+Thetoyking91@users.noreply.github.com>
2025-03-23 16:03:41 -0700

    Created plugin

---
+++ src/userplugins/appUtils/index.ts
@@ -0,0 +1,24 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "AppUtils",
+    description: "A collection of utilities that use a discord app",
+    authors: [{
+        name: "Ryan McCoy",
+        id: 526299701553922059n
+    }],
+
+    patches: [],
+    start() {
+
+    },
+    stop() {
+
+    },
+});
